c95b54b67fcc945c2095c440a5256af1e0d0037b
Mateus Adada <mateusadada@gmail.com>
2023-09-20 20:48:55 -0300

    Update principal.py
    Added the final code

--- solved_exercises/VPL - Virtual Programming Lab/5 - Pilha encadeada/principal.py
+++ solved_exercises/VPL - Virtual Programming Lab/5 - Pilha encadeada/principal.py
@@ -1,8 +1,8 @@
-#1- Incluir uma nova letra na pilha (não imprimir nada)
-#2- Excluir a letra do topo da pilha (ou em caso de pilha vazia, imprimir "Erro")
-#3- Imprimir a letra do topo da pilha (preservar a pilha, ou em caso de pilha vazia imprimir "Vazio")
-#4- Imprimir todas as letras da pilha (preservar a pilha, ou em caso de pilha vazia imprimir "Vazio")
-#5- Excluir todas as letras da pilha (não imprimir nada)
+# 1- Incluir uma nova letra na pilha (não imprimir nada)
+# 2- Excluir a letra do topo da pilha (ou em caso de pilha vazia, imprimir "Erro")
+# 3- Imprimir a letra do topo da pilha (preservar a pilha, ou em caso de pilha vazia imprimir "Vazio")
+# 4- Imprimir todas as letras da pilha (preservar a pilha, ou em caso de pilha vazia imprimir "Vazio")
+# 5- Excluir todas as letras da pilha (não imprimir nada)
 
 
 class No:
@@ -11,11 +11,41 @@ class No:
         self.dado = dado
         self.anterior = None
 
+
 class Pilha:
 
     def __init__(self):
-        self.minhaPilha = None
         self.topo = None
+
+    def empilhar(self, valor):
+        novoNo = No(valor)
+        novoNo.anterior = self.topo
+        self.topo = novoNo
+
+    def imprimir_tudo(self):
+        if self.topo is None:
+            print('Vazio')
+        else:
+            auxiliar = self.topo
+            while auxiliar is not None:
+                print(auxiliar.dado)
+                auxiliar = auxiliar.anterior
+
+    def imprimir_topo(self):
+        if self.topo is None:
+            print('Vazio')
+        else:
+            print(self.topo.dado)
+
+    def desempilhar(self):
+        if self.topo is None:
+            print('Erro')
+        else:
+            self.topo = self.topo.anterior
+
+    def apagar_tudo(self):
+        while self.topo is not None:
+            self.topo = self.topo.anterior
 
 
 def main():
@@ -23,8 +53,8 @@ def main():
     p1 = Pilha()
     
     while True:
-        #opcao = int(input("\n1- Empilhar\n2– Desempilhar\n3- Sair\n\nOpção: "))
-        #print("\n1- Empilhar\n2– Desempilhar\n3- Sair\n\nOpção: ")
+        # opcao = int(input("\n1- Empilhar\n2– Desempilhar\n3- Sair\n\nOpção: "))
+        # print("\n1- Empilhar\n2– Desempilhar\n3- Sair\n\nOpção: ")
         print("")
         print("1- Empilhar")
         print("2– Desempilhar")
@@ -32,19 +62,25 @@ def main():
         print("4- Imprimir tudo")
         print("5- Excluir tudo")
         print("5- Sair")
-        print("")
-        print("Opção: ")
+        print("Opção: ", end='')
         opcao = int(input())
+        print("")
+
         if opcao == 1:
-            pass
+            p1.empilhar(str(input('Digite um valor: ')))
+
         elif opcao == 2:
-            pass
+            p1.desempilhar()
+
         elif opcao == 3:
-            pass
+            p1.imprimir_topo()
+
         elif opcao == 4:
-            pass
+            p1.imprimir_tudo()
+
         elif opcao == 5:
-            pass
+            p1.apagar_tudo()
+
         else:
             break
     
