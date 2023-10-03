a8a59e4a27e1058a640ed9f9da0bc06219527fa0
Mateus Adada <mateusadada@gmail.com>
2023-10-02 23:21:35 -0300

    Created VPL 7 and 8
    Added files via upload

---
+++ solved_exercises/VPL - Virtual Programming Lab/7 - Lista simplesmente encadeada/principal.py
@@ -0,0 +1,66 @@
+class No:
+
+    def __init__(self, dado):
+        self.dado = dado
+        self.proximo = None
+
+
+class Lista:
+
+    def __init__(self):
+        self.inicio = None
+        # self.fim = None #PROIBIDO!!!!
+
+
+def main():
+    L1 = Lista()
+    
+    while True:
+        print("")
+        print("1- Inserir Inicio")
+        print("2– Remover Inicio")
+        print("3- Inserir Final")
+        print("4- Remover Final")
+        print("5- Imprimir Tudo")
+        print("6- Excluir Tudo")
+        print("7- Inserir na Posicao")
+        print("8- Remover da Posicao")
+        print("9- Imprimir da Posicao")
+        print("10- Sair")
+        print("")
+        print("Opção: ")
+        opcao = int(input())
+
+        if opcao == 1:
+            pass
+
+        elif opcao == 2:
+            pass
+
+        elif opcao == 3:
+            pass
+
+        elif opcao == 4:
+            pass
+
+        elif opcao == 5:
+            pass
+
+        elif opcao == 6:
+            pass
+
+        elif opcao == 7:
+            pass
+
+        elif opcao == 8:
+            pass
+
+        elif opcao == 9:
+            pass
+
+        else:
+            break
+    
+
+if __name__ == "__main__":
+    main()
