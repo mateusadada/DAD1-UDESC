e400308494f4e950bf4fa122003a308f5febb80a
Mateus Adada <mateusadada@gmail.com>
2023-10-03 22:52:57 -0300

    Update principal.py
    Added part of code

--- solved_exercises/VPL - Virtual Programming Lab/7 - Lista simplesmente encadeada/principal.py
+++ solved_exercises/VPL - Virtual Programming Lab/7 - Lista simplesmente encadeada/principal.py
@@ -11,6 +11,33 @@ class Lista:
         self.inicio = None
         # self.fim = None #PROIBIDO!!!!
 
+    def inserir_inicio(self, valor):
+        pass
+
+    def remover_inicio(self):
+        pass
+
+    def inserir_final(self, valor):
+        pass
+
+    def remover_final(self):
+        pass
+
+    def imprimir_tudo(self):
+        pass
+
+    def excluir_tudo(self):
+        pass
+
+    def inserir_na_posicao(self, posicao, valor):
+        pass
+
+    def remover_da_posicao(self, posicao):
+        pass
+
+    def imprimir_da_posicao(self, posicao):
+        pass
+
 
 def main():
     L1 = Lista()
@@ -23,31 +50,31 @@ def main():
         print("4- Remover Final")
         print("5- Imprimir Tudo")
         print("6- Excluir Tudo")
-        print("7- Inserir na Posicao")
-        print("8- Remover da Posicao")
-        print("9- Imprimir da Posicao")
+        print("7- Inserir na Posicao") #
+        print("8- Remover da Posicao") #
+        print("9- Imprimir da Posicao") #
         print("10- Sair")
         print("")
         print("Opção: ")
         opcao = int(input())
 
         if opcao == 1:
-            pass
+            L1.inserir_inicio(str(input('Digite um valor: ')))
 
         elif opcao == 2:
-            pass
+            L1.remover_inicio()
 
         elif opcao == 3:
-            pass
+            L1.inserir_final(str(input('Digite um valor: ')))
 
         elif opcao == 4:
-            pass
+            L1.remover_final()
 
         elif opcao == 5:
-            pass
+            L1.imprimir_tudo()
 
         elif opcao == 6:
-            pass
+            L1.excluir_tudo()
 
         elif opcao == 7:
             pass
