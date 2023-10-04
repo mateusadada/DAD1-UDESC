0f5371742f09c597e9def2cb3000725d4ecfaf89
Mateus Adada <mateusadada@gmail.com>
2023-10-04 20:37:08 -0300

    Update principal.py
    Added the code

--- solved_exercises/VPL - Virtual Programming Lab/8 - Lista duplamente encadeada/principal.py
+++ solved_exercises/VPL - Virtual Programming Lab/8 - Lista duplamente encadeada/principal.py
@@ -12,10 +12,98 @@ class Lista:
         self.inicio = None
         self.fim = None
 
+    def inserir_inicio(self, valor):
+        novoNo = No(valor)
+        novoNo.proximo = self.inicio
+        if self.inicio is None:
+            self.fim = novoNo
+        else:
+            self.inicio.anterior = novoNo
+
+        self.inicio = novoNo
+
+    def remover_inicio(self):
+        if self.inicio is None:
+            print('Erro')
+        else:
+            self.inicio = self.inicio.proximo
+            if self.inicio is not None:
+                self.inicio.anterior = None
+            else:
+                self.fim = None
+
+    def inserir_final(self, valor):
+        novoNo = No(valor)
+        if self.inicio is None and self.fim is None:
+            self.inicio = novoNo
+            self.fim = novoNo
+        else:
+            self.fim.proximo = novoNo
+            novoNo.proximo = None
+            novoNo.anterior = self.fim
+            self.fim = novoNo
+
+    def remover_final(self):
+        if self.inicio is None and self.fim is None:
+            print('Vazia')
+        else:
+            self.fim.anterior.proximo = None
+            self.fim = self.fim.proximo
+
+    def imprimir_tudo(self):
+        if self.inicio is None:
+            print('Vazio')
+        else:
+            auxiliar = self.inicio
+            while auxiliar is not None:
+                print(auxiliar.dado, end=' ')
+                auxiliar = auxiliar.proximo
+
+    def excluir_tudo(self):
+        self.inicio = None
+        self.fim = None
+
+    def inserir_na_posicao(self, posicao, valor):
+        if self.inicio is None or posicao == 0:
+            self.inserir_inicio(valor)
+        else:
+            atual = self.inicio
+            for p in range(posicao):
+                if atual is not None:
+                    atual = atual.proximo
+            novoNo = No(valor)
+            novoNo.proximo = atual
+            novoNo.anterior = atual.anterior.proximo
+            atual.anterior.proximo = novoNo
+            atual.anterior = novoNo
+
+    def remover_da_posicao(self, posicao):
+        if posicao == 0:
+            self.remover_inicio()
+        else:
+            auxiliar = self.inicio
+            posicao_atual = 0
+            while auxiliar is not None:
+                if posicao_atual == posicao:
+                    auxiliar.anterior.proximo = auxiliar.proximo
+                    auxiliar.proximo.anterior = auxiliar.anterior
+                auxiliar = auxiliar.proximo
+                posicao_atual += 1
+
+    def imprimir_da_posicao(self, posicao):
+        auxiliar = self.inicio
+        posicao_atual = 0
+        while auxiliar is not None:
+            if posicao_atual == posicao:
+                print(auxiliar.dado)
+                return
+            auxiliar = auxiliar.proximo
+            posicao_atual += 1
+
 
 def main():
     L1 = Lista()
-    
+
     while True:
         print("")
         print("1- Inserir Inicio")
@@ -33,35 +121,37 @@ def main():
         opcao = int(input())
 
         if opcao == 1:
-            pass
+            L1.inserir_inicio(str(input('Digite um valor: ')))
 
         elif opcao == 2:
-            pass
+            L1.remover_inicio()
 
         elif opcao == 3:
-            pass
+            L1.inserir_final(str(input('Digite um valor: ')))
 
         elif opcao == 4:
-            pass
+            L1.remover_final()
 
         elif opcao == 5:
-            pass
+            L1.imprimir_tudo()
 
         elif opcao == 6:
-            pass
+            L1.excluir_tudo()
 
         elif opcao == 7:
-            pass
+            posicao = int(input('Digite uma posição'))
+            valor = str(input('Digite um valor'))
+            L1.inserir_na_posicao(posicao, valor)
 
         elif opcao == 8:
-            pass
+            L1.remover_da_posicao(int(input('Digite uma posição: ')))
 
         elif opcao == 9:
-            pass
+            L1.imprimir_da_posicao(int(input('Digite uma posição: ')))
 
         else:
             break
-    
+
 
 if __name__ == "__main__":
     main()
