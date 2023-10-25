e368d6aa52608dd654035af279391e6db893dbc1
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-10-25 12:13:59 -0300

    Update principal.py
    Added the code

--- solved_exercises/VPL - Virtual Programming Lab/10 - Recursão II/principal.py
+++ solved_exercises/VPL - Virtual Programming Lab/10 - Recursão II/principal.py
@@ -1,16 +1,18 @@
-def mdc(numero_1, numero_2):
-    # Coloque seu código aqui
-    pass
+def mdc(numero_1, numero_2, comum):
+    if numero_1 % comum == 0 and numero_2 % comum == 0:
+        return comum
+    else:
+        return mdc(numero_1, numero_2, comum - 1)
 
 
 def main():
     numero_1 = int(input('Digite um número: '))
     numero_2 = int(input('Digite um número: '))
 
-    resultado = mdc(numero_1, numero_2)
+    resultado = mdc(numero_1, numero_2, numero_2)
     
-    print("Resultado: ", resultado)
+    print('Resultado: ', resultado)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
